1c2faa2d76935305535425513278c1ae88c65812
Operation79 <manc4life2k7@hotmail.com>
2011-03-23 18:30:06 -0700

    Added copyright notice

--- error.php
+++ error.php
@@ -1,4 +1,16 @@
 <?php
+/*-------------------------------------------------------+
+| AneCMS
+| Copyright (C) 2010
+| http://anecms.com
++--------------------------------------------------------+
+| Filename: error.php
+| Author: Gonçalo Margalho
++--------------------------------------------------------+
+| Removal of this copyright header is strictly 
+| prohibited without written permission from 
+| Gonçalo Margalho.
++--------------------------------------------------------*/
 define('ANECMS', true);
 include './system/pages/essential.php';
 

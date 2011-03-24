e64eec70fc6a0b3e410ee857968343e8667809e8
Operation79 <manc4life2k7@hotmail.com>
2011-03-23 18:32:41 -0700

    Added Copyright notice

--- rss.php
+++ rss.php
@@ -1,4 +1,16 @@
 <?php
+/*-------------------------------------------------------+
+| AneCMS
+| Copyright (C) 2010
+| http://anecms.com
++--------------------------------------------------------+
+| Filename: rss.php
+| Author: Gonçalo Margalho
++--------------------------------------------------------+
+| Removal of this copyright header is strictly 
+| prohibited without written permission from 
+| Gonçalo Margalho.
++--------------------------------------------------------*/
 define('ANECMS', true);
 define('RSS', true);
 include './system/pages/essential.php';

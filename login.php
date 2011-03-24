18a29320bde2bc1daa22e69a6eb75a2925b797b0
Operation79 <manc4life2k7@hotmail.com>
2011-03-23 18:31:42 -0700

    Added copyright notice

--- login.php
+++ login.php
@@ -1,4 +1,16 @@
 <?php
+/*-------------------------------------------------------+
+| AneCMS
+| Copyright (C) 2010
+| http://anecms.com
++--------------------------------------------------------+
+| Filename: login.php
+| Author: Gonçalo Margalho
++--------------------------------------------------------+
+| Removal of this copyright header is strictly 
+| prohibited without written permission from 
+| Gonçalo Margalho.
++--------------------------------------------------------*/
 define('ANECMS', true);
 define('ACCESS', true);
 define('LOGIN', true);
